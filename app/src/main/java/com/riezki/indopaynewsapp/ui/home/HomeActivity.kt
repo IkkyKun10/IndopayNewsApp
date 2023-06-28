4d46e607d7f763d928cbab2f49ce20cf65946531
riezkymaisyar <riezkymaisyar10@gmail.com>
2023-06-29 00:05:41 +0700

    Mendapatkan nama lokasi kecamatan device

--- app/src/main/java/com/riezki/indopaynewsapp/ui/home/HomeActivity.kt
+++ app/src/main/java/com/riezki/indopaynewsapp/ui/home/HomeActivity.kt
@@ -1,17 +1,30 @@
 package com.riezki.indopaynewsapp.ui.home
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.CancellationTokenSource
+import com.riezki.indopaynewsapp.R
 import com.riezki.indopaynewsapp.adapter.NewsAdapter
 import com.riezki.indopaynewsapp.databinding.ActivityHomeBinding
 import com.riezki.indopaynewsapp.ui.detail.DetailActivity
 import com.riezki.indopaynewsapp.utils.Resource
 import com.riezki.indopaynewsapp.utils.ViewModelFactory
+import java.util.Locale
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
@@ -20,10 +33,15 @@ class HomeActivity : AppCompatActivity() {
         ViewModelFactory.getInstance(this)
     }
 
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private var currentLocation: Location? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
         newsAdapter = NewsAdapter { news ->
             val intent = Intent(this, DetailActivity::class.java)
             intent.putExtra(DetailActivity.INTENT_TO_DETAIL, news)
@@ -32,6 +50,18 @@ class HomeActivity : AppCompatActivity() {
         setRecyclerView()
 
         showNewsHeadline()
+
+        getMyLastLocation()
+
+        binding.shareLocBtn.setOnClickListener {
+            binding.progressBarLoc.visibility = View.VISIBLE
+            getMyLastLocation()
+
+            binding.latText.text = getString(R.string.latitude_info, currentLocation?.latitude.toString())
+            binding.lonText.text = getString(R.string.longitude_info, currentLocation?.longitude.toString())
+            binding.cityName.text = getString(R.string.name_of_city, getCityName(currentLocation?.latitude ?: 0.0, currentLocation?.longitude ?: 0.0))
+            binding.progressBarLoc.visibility = View.GONE
+        }
     }
 
     private fun showNewsHeadline() {
@@ -55,7 +85,7 @@ class HomeActivity : AppCompatActivity() {
             }
         }
     }
-    
+
     private fun setRecyclerView() {
         binding.rvNews.apply {
             layoutManager = LinearLayoutManager(this@HomeActivity)
@@ -63,5 +93,75 @@ class HomeActivity : AppCompatActivity() {
             clipToPadding = false
         }
 
+    }
+
+    //permission location
+    private fun checkPermission(permission: String): Boolean {
+        return this.let {
+            ContextCompat.checkSelfPermission(
+                it, permission
+            )
+        } == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun getMyLastLocation() {
+        if (checkPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+            && checkPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+        ) {
+            fusedLocationProviderClient.getCurrentLocation(
+                PRIORITY_HIGH_ACCURACY,
+                CancellationTokenSource().token
+            ).addOnSuccessListener { loc: Location? ->
+                if (loc != null) {
+                    currentLocation = loc
+
+                }
+            }
+                .addOnFailureListener {
+                    Toast.makeText(
+                        this, "Failed on getting current location",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+        } else {
+            requestPermissionLauncher.launch(
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            )
+        }
+
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permission ->
+        when {
+            permission[Manifest.permission.ACCESS_FINE_LOCATION] ?: false -> {
+                getMyLastLocation()
+            }
+
+            permission[Manifest.permission.ACCESS_COARSE_LOCATION] ?: false -> {
+                getMyLastLocation()
+            }
+
+            else -> {
+
+            }
+        }
+    }
+
+    private fun getCityName(latitude: Double, longitude: Double) : String {
+        var cityName: String? = ""
+        var countryName: String? = ""
+        var geocoder: Geocoder? = Geocoder(this, Locale.getDefault())
+        var address = geocoder?.getFromLocation(latitude, longitude, 3)
+
+        cityName = address?.get(0)?.locality
+        countryName = address?.get(0)?.countryName
+        Log.d("Debug:", "Your City: $cityName ; your Country $countryName")
+
+        return cityName as String
     }
 }
